31fe34b880b3bf61ea69f2b274abf62ef1d6b2c8
John Robert Maizo <johnrobertmaizo@gmail.com>
2024-09-11 08:55:16 +0800

    update

--- src/components/styles/SmallLoader.jsx
+++ src/components/styles/SmallLoader.jsx
@@ -2,6 +2,7 @@
 const SmallLoader = ({ width, height }) => {
   return (
     <span
+      aria-label="Loading..."
       className={`${width && height ? `w-${width} h-${height}` : "h-5 w-5"} animate-spin rounded-full border-4 border-solid border-[#3b82f6] border-t-transparent`}
     ></span>
   );

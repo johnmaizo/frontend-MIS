19cda6717b99b8878f51261c8fca7842bdc51185
John Robert Maizo <johnrobertmaizo@gmail.com>
2024-09-13 17:38:47 +0800

    adding comment

--- src/components/reuseable/GetUniqueValues.jsx
+++ src/components/reuseable/GetUniqueValues.jsx
@@ -1,3 +1,10 @@
+/**
+ * Returns an array of unique values with a label containing the value
+ * @param {object[]} data - Array of objects containing the value
+ * @param {string} uniqueKey - The key that uniquely identifies each value
+ * @returns {object[]} An array of objects with 'value' and 'label' properties
+ */
+
 export const getUniqueCodes = (data, uniqueKey) => {
   return [
     ...new Map(
@@ -32,6 +39,12 @@ export const getUniqueCourseCodes = (data, uniqueKey) => {
   ];
 };
 
+/**
+ * Returns an array of unique strings from the given data array by the specified key.
+ * @param {object[]} data - Array of objects containing the key
+ * @param {string} uniqueKey - The key that uniquely identifies each value
+ * @returns {string[]} An array of unique strings
+ */
 export const getUniqueStringsByProperty = (data, uniqueKey) => {
   return [...new Set(data.map((item) => item[uniqueKey]))];
 };

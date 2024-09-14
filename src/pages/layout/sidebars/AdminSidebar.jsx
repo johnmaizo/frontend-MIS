6762eea3b42ad98d53d366ac0f7150a355aaffde
John Robert Maizo <johnrobertmaizo@gmail.com>
2024-09-14 22:24:11 +0800

    adding new nav "Enrollments"

--- src/pages/layout/sidebars/AdminSidebar.jsx
+++ src/pages/layout/sidebars/AdminSidebar.jsx
@@ -127,19 +127,91 @@ const AdminSidebar = ({
             ENROLLMENT SECTION
           </h3>
 
-          <li>
-            <NavLink
-              to="/enrollments"
-              className={`group relative flex items-start gap-2.5 rounded-sm px-4 py-2 font-medium duration-300 ease-in-out hover:bg-gray dark:text-bodydark1 dark:hover:bg-meta-4 ${
-                (pathname === "/enrollments" ||
-                  pathname.includes("enrollments")) &&
-                "bg-gray text-primary underline underline-offset-4 dark:bg-meta-4"
-              }`}
-            >
-              <EnrollmentIcon />
-              Enrollment Application
-            </NavLink>
-          </li>
+          <SidebarLinkGroup
+            activeCondition={
+              pathname === "/enrollments" || pathname.includes("enrollments")
+            }
+          >
+            {(handleClick, open) => {
+              return (
+                <React.Fragment>
+                  <NavLink
+                    to="/courses/courses-list"
+                    className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium hover:bg-gray dark:text-bodydark1 dark:hover:bg-meta-4 ${
+                      (pathname === "/courses" ||
+                        pathname.includes("courses")) &&
+                      "bg-gray text-primary dark:bg-meta-4"
+                    }`}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      sidebarExpanded
+                        ? handleClick()
+                        : setSidebarExpanded(true);
+                    }}
+                  >
+                    <EnrollmentIcon />
+                    Enrollments
+                    <svg
+                      className={`absolute right-4 top-1/2 -translate-y-1/2 fill-current ${
+                        open && "rotate-180"
+                      }`}
+                      width="20"
+                      height="20"
+                      viewBox="0 0 20 20"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M4.41107 6.9107C4.73651 6.58527 5.26414 6.58527 5.58958 6.9107L10.0003 11.3214L14.4111 6.91071C14.7365 6.58527 15.2641 6.58527 15.5896 6.91071C15.915 7.23614 15.915 7.76378 15.5896 8.08922L10.5896 13.0892C10.2641 13.4147 9.73651 13.4147 9.41107 13.0892L4.41107 8.08922C4.08563 7.76378 4.08563 7.23614 4.41107 6.9107Z"
+                        fill=""
+                      />
+                    </svg>
+                  </NavLink>
+                  {/* <!-- Dropdown Menu Start --> */}
+                  <div
+                    className={`translate transform overflow-hidden ${
+                      !open && "hidden"
+                    }`}
+                  >
+                    <ul className="mb-5.5 mt-4 flex flex-col gap-2.5 pl-6">
+                      <li>
+                        <NavLink
+                          to="/enrollments/enrollment-application"
+                          className={({ isActive }) =>
+                            `group relative flex items-center gap-2.5 rounded-md px-4 font-medium underline-offset-4 duration-300 ease-in-out hover:underline dark:text-bodydark1 ${
+                              (pathname ===
+                                "/enrollments/enrollment-application" ||
+                                pathname.includes("enrollment-application")) &&
+                              "!underline "
+                            }` + (isActive && "text-primary dark:!text-white")
+                          }
+                        >
+                          Enrollment Application
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink
+                          to="/courses/program-courses"
+                          className={({ isActive }) =>
+                            `group relative flex items-center gap-2.5 rounded-md px-4 font-medium underline-offset-4 duration-300 ease-in-out hover:underline dark:text-bodydark1 ${
+                              (pathname === "/courses/program-courses" ||
+                                pathname.includes("program-courses")) &&
+                              "!underline "
+                            }` + (isActive && "text-primary dark:!text-white")
+                          }
+                        >
+                          Program Courses
+                        </NavLink>
+                      </li>
+                    </ul>
+                  </div>
+                  {/* <!-- Dropdown Menu End --> */}
+                </React.Fragment>
+              );
+            }}
+          </SidebarLinkGroup>
 
           <h3 className="my-2 ml-4 mt-6 text-sm font-semibold text-bodydark2">
             DEPARTMENT SECTION

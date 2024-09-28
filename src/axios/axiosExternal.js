854460c6d316bf8f0582f5ab1c6f7920373c377b
John Robert Maizo <johnrobertmaizo@gmail.com>
2024-09-28 08:50:49 +0800

    update

--- src/axios/axiosExternal.js
+++ src/axios/axiosExternal.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosExternal = axios.create({
-  baseURL: "https://afknon.pythonanywhere.com", // Set the base URL for external API
+  baseURL: import.meta.env.VITE_REACT_APP_BASE_URL_MHAFRIC, // Set the base URL for external API
   headers: {
     "Content-Type": "application/json", // Add any necessary headers here
   },

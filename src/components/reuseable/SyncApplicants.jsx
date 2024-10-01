4a47e6826b2cd9761127117f3d61d596553c9096
John Robert Maizo <johnrobertmaizo@gmail.com>
2024-10-01 14:49:01 +0800

    update

--- src/components/reuseable/SyncApplicants.jsx
+++ src/components/reuseable/SyncApplicants.jsx
@@ -8,11 +8,19 @@ import { useEnrollment } from "../context/EnrollmentContext";
 
 const SyncApplicants = () => {
   const [loading, setLoading] = useState(false);
-
   const { fetchApplicants } = useEnrollment();
+  let longProcessToastId = null;
+  let timer = null;
 
   const handleAction = async () => {
     setLoading(true);
+
+    // Set a timeout to show the loading toast after 7 seconds if the process is still running
+    timer = setTimeout(() => {
+      longProcessToastId = toast.loading("This may take a while...", {
+        position: "bottom-right",
+      });
+    }, 7000);
 
     try {
       const response = await axios.get("/enrollment/fetch-applicant-data");
@@ -44,16 +52,26 @@ const SyncApplicants = () => {
         });
       }
       setLoading(false);
+    } finally {
+      // Clear the timer if the process finishes before 7 seconds
+      clearTimeout(timer);
+
+      // If the loading toast was shown, dismiss it
+      if (longProcessToastId) {
+        toast.dismiss(longProcessToastId);
+      }
     }
   };
 
   return (
     <Button
-      className={`inline-flex w-full gap-2 ${loading ? "cursor-not-allowed bg-green-800" : ""} bg-green-600 hover:bg-green-700`}
+      className={`inline-flex w-full gap-2 ${
+        loading ? "cursor-not-allowed bg-green-800" : ""
+      } bg-green-600 hover:bg-green-700`}
       onClick={handleAction}
       disabled={loading}
     >
-      <RefreshCcwIcon className={`${loading ? "animate-spin" : ""}`} />
+      <RefreshCcwIcon className={`${loading ? "animate-reverse-spin" : ""}`} />
       {loading ? "Syncing Applicants..." : "Sync Applicants"}
     </Button>
   );

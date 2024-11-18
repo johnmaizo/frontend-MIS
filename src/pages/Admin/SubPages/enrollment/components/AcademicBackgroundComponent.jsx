07afffdc87e756ffa08b12ec4ffcc9c29ef99955
John Robert Maizo <johnrobertmaizo@gmail.com>
2024-11-18 15:17:37 +0800

    update

--- src/pages/Admin/SubPages/enrollment/components/AcademicBackgroundComponent.jsx
+++ src/pages/Admin/SubPages/enrollment/components/AcademicBackgroundComponent.jsx
@@ -201,7 +201,7 @@ const AcademicBackgroundComponent = () => {
             <option value="Second Year">Second Year</option>
             <option value="Third Year">Third Year</option>
             <option value="Fourth Year">Fourth Year</option>
-            <option value="Fifth Year">Fifth Year</option>
+            {/* <option value="Fifth Year">Fifth Year</option> */}
           </select>
           {errors.yearLevel && (
             <span className="text-sm font-medium text-red-600">
